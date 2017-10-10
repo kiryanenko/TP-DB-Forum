73eaff6eab3d65f2538025cac1872d41abbac504
kiryanenko <KiryanenkoAV@mail.ru>
2017-10-11 02:26:33 +0300

    Получение информации о пользователе форума по его имени

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
-create table person (
-  id serial primary key,
-  nickname CITEXT COLLATE "en_US.utf8" not null UNIQUE,
+CREATE TABLE person (
+  id SERIAL PRIMARY KEY,
+  nickname CITEXT COLLATE "en_US.utf8" NOT NULL UNIQUE,
   fullname text NOT NULL,
   email CITEXT COLLATE "en_US.utf8" NOT NULL UNIQUE,
   about text
