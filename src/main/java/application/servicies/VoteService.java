a4482409ecd9d33f58ba40825b46dfe230a571da
kiryanenko <KiryanenkoAV@mail.ru>
2017-11-28 03:15:27 +0300

    Remove transactions

--- src/main/java/application/servicies/VoteService.java
+++ src/main/java/application/servicies/VoteService.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-@Transactional
 public class VoteService {
     private final NamedParameterJdbcTemplate template;
     private ThreadService threadService;
